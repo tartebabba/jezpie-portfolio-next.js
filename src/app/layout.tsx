acaa1de851b28e2a73b4d20f73a9b89bec44e2c2
tartebabba <jez.piedad@gmail>
2024-06-18 12:37:07 +0100

    add initials to tab icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata = {
   title: 'Jez Pie - Software Engineering Portfolio',
   description:
     'Jez Pie - My software engineering portfolio and programming blog',
-  // icons: [{ rel: "icon", url: "/favicon.ico" }],
+  icons: [{ rel: 'icon', url: '/favicon.ico' }],
   creator: 'Jez Pie',
   category: 'technology',
 };
@@ -18,9 +18,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
+      <head>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      </head>
       <body className="flex h-screen flex-col justify-between dark:bg-slate-950 dark:text-slate-50">
         <div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
           <TopNavBar />
