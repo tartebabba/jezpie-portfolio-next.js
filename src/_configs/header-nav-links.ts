5526625ac14d569ee952ea563b55e332c66459c1
tartebabba <jez.piedad@gmail>
2024-06-20 10:42:50 +0100

    update post route

--- src/_configs/header-nav-links.ts
+++ src/_configs/header-nav-links.ts
@@ -1,6 +1,6 @@
 export const headerNavLinks = [
   { href: '/me', title: 'about me' },
   { href: '/projects', title: 'projects' },
-  // { href: '/blog', title: 'blog' },
+  { href: '/posts', title: 'posts' },
   // { href: '/blog/tags', title: 'tags' },
 ];
--- src/app/_components/top-navigation-bar.tsx
+++ src/app/_components/top-navigation-bar.tsx
@@ -5,7 +5,12 @@ import Typewriter from 'typewriter-effect';
 import { headerNavLinks } from '~/_configs/header-nav-links';
 
 export default function TopNavBar() {
-  const pathname = usePathname();
+
+  const pathname = usePathname().startsWith('/posts/')
+    ? '/posts'
+    : usePathname();
+
+  console.log(pathname);
 
   return (
     <header className="items-center justify-between py-10 md:flex">
