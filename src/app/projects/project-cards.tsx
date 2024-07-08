d78d74061b8d808ce91c508d6976d03397b3f490
tartebabba <jez.piedad@gmail>
2024-07-08 10:42:22 +0100

    fix project cards

--- src/app/projects/project-cards.tsx
+++ src/app/projects/project-cards.tsx
@@ -10,56 +10,61 @@ import CardTopBorderFlex from '../_components/card-top-border-flex';
 
 export default function ProjectCards() {
   return (
-    <div className="my-1 grid grid-cols-2">
-      {projects.map((project) => (
-        <div
-          key={project.id}
-          className="m-4 rounded-lg border-2 border-gray-300 p-3 dark:border-gray-600"
-        >
-          {/* <img
+    <div className="container py-12">
+      <div className="-m-4 flex flex-wrap">
+        {projects.map((project) => (
+          <div
+            key={project.id}
+            className="m-4 h-full w-full rounded-lg border-2 border-gray-300 p-3 dark:border-gray-600"
+          >
+            {/* <img
             src={project.image}
             alt={project.name}
             width={500}
             height={500}
             className="aspect-video w-full rounded-lg object-cover"
           /> */}
-          <div className="my-2 flex items-center justify-between px-1">
-            {project.complete ? (
-              <div style={{ color: 'green' }} className="m-1 flex items-center">
-                <CheckCircledIcon className="fill-green mr-2" />
-                <p className="text-sm">Complete</p>
-              </div>
-            ) : (
-              <div
-                style={{ color: 'orange' }}
-                className="m-1 flex items-center"
-              >
-                <ExclamationTriangleIcon className="fill-red mr-2" />
-                <p className="text-sm">In Progress</p>
-              </div>
-            )}
-            <div className="flex space-x-2">
-              <a href={project.github}>
-                <GitHubLogoIcon className="fill-green" />
-              </a>
-              {project.website && (
-                <a href={project.website} className="text-xl">
-                  <ExternalLinkIcon />
-                </a>
+            <div className="my-2 flex items-center justify-between px-1">
+              {project.complete ? (
+                <div
+                  style={{ color: 'green' }}
+                  className="m-1 flex items-center"
+                >
+                  <CheckCircledIcon className="fill-green mr-2" />
+                  <p className="text-sm">Complete</p>
+                </div>
+              ) : (
+                <div
+                  style={{ color: 'orange' }}
+                  className="m-1 flex items-center"
+                >
+                  <ExclamationTriangleIcon className="fill-red mr-2" />
+                  <p className="text-sm">In Progress</p>
+                </div>
               )}
+              <div className="flex space-x-2">
+                <a href={project.github}>
+                  <GitHubLogoIcon className="fill-green" />
+                </a>
+                {project.website && (
+                  <a href={project.website} className="text-xl">
+                    <ExternalLinkIcon />
+                  </a>
+                )}
+              </div>
             </div>
+            <h2 className="text-3xl sm:text-2xl font-bold hover:text-[hsl(280,100%,70%)]">
+              {project.name}
+            </h2>
+            <p className="my-2">{project.description}</p>
+            <CardTopBorderFlex>
+              {project.tags.map((tag) => (
+                <Tags key={tag} tag={tag} />
+              ))}
+            </CardTopBorderFlex>
           </div>
-          <h2 className="text-3xl font-bold hover:text-[hsl(280,100%,70%)]">
-            {project.name}
-          </h2>
-          <p className="my-2">{project.description}</p>
-          <CardTopBorderFlex>
-            {project.tags.map((tag) => (
-              <Tags key={tag} tag={tag} />
-            ))}
-          </CardTopBorderFlex>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
--- src/app/projects/projects.tsx
+++ src/app/projects/projects.tsx
@@ -5,7 +5,7 @@ export default function Projects() {
   return (
     <main className="mb-12 flex w-full flex-col gap-x-12 xl:flex-row xl:content-evenly">
       <div className="my-1 pt-4">
-        <h1 className="md:leading-14 pb-6 text-2xl font-extrabold leading-9 tracking-tight sm:text-4xl sm:leading-10 md:text-6xl">
+        <h1 className="md:leading-14 pb-6 text-3xl font-extrabold leading-9 tracking-tight sm:text-4xl sm:leading-10 md:text-6xl">
           My Projects
         </h1>
         <ContainerTopBorder>
